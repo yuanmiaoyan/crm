24a6072d703e35e0f4e11ef7f1009d74d83b1d03
miao <979476293@qq.com>
2016-07-05 15:13:42 +0800

    name change

--- 20160616_CRM客户管理系统/js/add.js
+++ 20160616_CRM客户管理系统/js/add.js
@@ -75,7 +75,7 @@ submit.onclick = function () {
     //->增加
     objStr = JSON.stringify(obj);
     ajax({
-        url: "/addStuffInfo",
+        url: "/add",
         type: "post",
         data: objStr,
         cache:false,
